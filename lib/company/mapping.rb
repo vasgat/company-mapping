c41c8b300f9259698c428c5d25cf43eec266fe99
vasgat <vasgat@gmail.com>
2017-03-21 13:32:21 +0200

    mapping updated

--- lib/company/mapping.rb
+++ lib/company/mapping.rb
@@ -1,7 +1,18 @@
-require "company/mapping/version"
+require_relative "mapping/version"
 
 module Company
   module Mapping
-    # Your code goes here...
   end
 end
+
+require_relative 'mapping/document_utils/basic_tokenizer'
+require_relative 'mapping/document_utils/corpus'
+require_relative 'mapping/tfidf/tf/term_frequency'
+require 'securerandom'
+require_relative 'mapping/document_utils/text_document'
+require_relative 'mapping/tfidf/idf/inverse_document_frequency'
+require_relative 'mapping/tfidf/tf/normalized_term_frequency'
+require 'set'
+require_relative 'mapping/vector_similarity/cosine_similarity'
+require_relative 'mapping/tfidf/tfidf'
+require_relative 'mapping/company_mapper'
